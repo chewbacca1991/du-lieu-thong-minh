a51a2b6f2b5a26bbd8a7b5e92048c98b8f51cda1
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-01 19:00:58 +0330

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/du_lieu_thong_minh', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+  res.send('Chào mừng đến với Dữ Liệu Thông Minh!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server đang chạy trên cổng ${PORT}`);
+});

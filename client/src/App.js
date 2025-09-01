8a3ce53efe578be559584de631dd4b2d65ee3394
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-01 19:01:00 +0330

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dữ Liệu Thông Minh</h1>
+        <p>Phân tích và trực quan hóa dữ liệu cá nhân của bạn.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
